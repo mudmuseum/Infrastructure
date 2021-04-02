319b15a9ae3f1532cfc526c28de91fd25d957dfa
Nicholas Warner <nswarner2@gmail.com>
2021-04-02 13:32:19 -0400

    Forgot egress.. doh

--- mudmuseum/app/modules/security-group/main.tf
+++ mudmuseum/app/modules/security-group/main.tf
@@ -12,4 +12,11 @@ resource "aws_security_group" "security_group" {
       protocol    = "tcp"
     }
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
